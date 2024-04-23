f00711db670db51829a5f7b38b6aad9f3feebcdd
Greg Anderson <greg.anderson.cs@gmail.com>
2024-04-23 14:10:21 -0600

    Add fastapi app

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from schemas import Movie
 
 
@@ -6,3 +9,14 @@ movies: list[Movie] = [
     Movie(movie_id=561, name="Thor: Ragnarok", year=2017),
     Movie(movie_id=125, name="Iron Man", year=2008),
 ]
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # The origin of the frontend app
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

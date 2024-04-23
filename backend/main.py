9ce947b2b37e79f0ab145068e8d6161b9477b654
Greg Anderson <greg.anderson.cs@gmail.com>
2024-04-23 14:45:04 -0600

    Update models, add stubbed out APIs

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+import uuid
+
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
-from schemas import Movie
+from schemas import (
+    Movie,
+    CreateMovieRequest,
+    CreateMovieResponse,
+    UpdateMovieRequest,
+    UpdateMovieResponse,
+    DeleteMovieResponse,
+)
 
 
 movies: list[Movie] = [
-    Movie(movie_id=239, name="Spider-Man", year=2002),
-    Movie(movie_id=561, name="Thor: Ragnarok", year=2017),
-    Movie(movie_id=125, name="Iron Man", year=2008),
+    Movie(movie_id=uuid.uuid4(), name="Spider-Man", year=2002),
+    Movie(movie_id=uuid.uuid4(), name="Thor: Ragnarok", year=2017),
+    Movie(movie_id=uuid.uuid4(), name="Iron Man", year=2008),
 ]
 
 
@@ -15,8 +24,24 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # The origin of the frontend app
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/movies")
+async def get_movies() -> list[Movie]:
+    raise NotImplementedError
+
+@app.post("/movies")
+async def create_movie(new_movie: CreateMovieRequest) -> CreateMovieResponse:
+    raise NotImplementedError
+
+@app.put("/movies/{movie_id}")
+async def update_movie(movie_id: uuid.UUID, updated_movie: UpdateMovieRequest) -> UpdateMovieResponse:
+    raise NotImplementedError
+
+@app.delete("/movies/{movie_id}")
+async def delete_movie(movie_id: uuid.UUID) -> DeleteMovieResponse:
+    raise NotImplementedError
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,26 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 
-class Movie(BaseModel):
-    movie_id: int
+class BaseMovie(BaseModel):
     name: str
     year: int
+
+class CreateMovieRequest(BaseMovie):
+    pass
+
+class UpdateMovieRequest(BaseMovie):
+    pass
+
+class Movie(BaseMovie):
+    movie_id: UUID
+
+class CreateMovieResponse(BaseModel):
+    id: UUID
+
+class UpdateMovieResponse(BaseModel):
+    success: bool
+
+class DeleteMovieResponse(BaseModel):
+    success: bool
